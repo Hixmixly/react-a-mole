fa3feb77f397ef062714a7c584a670f69e83f4b6
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-07-20 16:23:20 -0700

    made adjustments to moleContainer, mole, and emptySlots now the code works

--- src/components/EmptySlot.js
+++ src/components/EmptySlot.js
@@ -1,21 +1,25 @@
-import { useEffect } from "react"
-import MoleHill from '../images/molehill (1).png'
+import { useEffect } from "react";
+import MoleHill from '../images/molehill.png'; 
 
 const EmptySlot = (props) => {
+  useEffect(() => {
+    let randSeconds = Math.ceil(Math.random() * 1000);
+    let timer = setTimeout(() => {
+      props.toggle(true);
+    }, randSeconds);
 
-    useEffect(() => {
-        let randSeconds = Math.ceil(Math.random() * 1000)
-        let timer = setTimeout(() => {
-            props.toggle(true)
-        }, randSeconds)
-        return () => clearTimeout(timer)
-    })
+    return () => clearTimeout(timer);
+  }, [props]); 
 
-    return (
-        <div>
-            <img style={{'width': '30vw'}} src={MoleHill} />
-        </div>
-    )
-}
+  return (
+    <div>
+      <img 
+        style={{ width: '30vw' }} 
+        src={MoleHill} 
+        alt="Empty Mole Hill"
+      />
+    </div>
+  );
+};
 
-export default EmptySlot
+export default EmptySlot;
--- src/components/Mole.js
+++ src/components/Mole.js
@@ -1,10 +1,10 @@
-
-import {useEffect} from 'react'
-import moleImg from '../images/mole (1).png'
+import { useEffect } from 'react'
+import moleImg from '../images/mole.png'
 
 const Mole = (props) => {
-    useEffect (() => {
-        let randSeconds = Math.ceil (Math.random () * 40000)
+
+    useEffect(() => {
+        let randSeconds = Math.ceil(Math.random() * 10000)
         let timer = setTimeout(() => {
             props.toggle(false)
         }, randSeconds)
@@ -13,12 +13,7 @@ const Mole = (props) => {
 
     return (
         <div>
-            <img style={{'width': '30vw'}}
-            src = {moleImg}
-            onClick={props.handleClick}
-            />
-
-
+            <img style={{'width': '30vw'}} src={moleImg} onClick={props.handleClick} />
         </div>
     )
 }
